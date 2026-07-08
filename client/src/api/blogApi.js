014de2b37a262b5328901dc6caa602cbe9c1eda1
ahash-p <ahashpsivam@gmail.com>
2026-07-08 20:33:24 +0530

    Connect frontend to deployed backend

--- client/src/api/blogApi.js
+++ client/src/api/blogApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/blogs",
+  baseURL: "https://ashblog-backend.onrender.com/api/blogs",
 });
 
 export default API;
